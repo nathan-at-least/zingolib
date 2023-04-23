437ef7db7e82f1af7f0cf54dd5232e6066f1c614
Nate Wilcox <nathan+dev@electriccoin.co>
2023-04-22 18:40:27 -0700

    Apply standard `cargo fmt` on `lightclient.rs`.

--- zingolib/src/lightclient.rs
+++ zingolib/src/lightclient.rs
@@ -1008,7 +1008,7 @@ impl LightClient {
                             "amount"       => wallet_transparent_value_delta,
                             "zec_price"    => wallet_transaction.zec_price.map(|p| (p * 100.0).round() / 100.0),
                             "address"      => address,
-                            "memo"         => None::<String> 
+                            "memo"         => None::<String>
                         })
                     }
                 }
